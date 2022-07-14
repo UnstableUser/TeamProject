bcd4f2fadc828edb2df3ccd81f7b9087d34e1712
27hjohnson1 <johnson.hyrum7@gmail.com>
2022-07-14 14:42:34 -0600

    trying new button

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-<script>
-         function show_hide(element)
-         {
-            var myAnswer = element.nextElementSibling;
-
-            var displaySetting = myAnswer.style.display;
-
-            var quizButton = element;
-
-            if(displaySetting=="inline-block"){
-                myAnswer.style.display = 'none';
-
-                quizButton.innerHTML = 'Show Answer';
-            }
-            else
-            {
-                myAnswer.style.display = 'inline-block';
-                quizButton.innerHTML = 'Hide Answer';
-            }
-         }
-</script>
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
